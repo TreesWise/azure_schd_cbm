00c72f207fe73259ce12a80fba25e7d83ff349db
TreesWise <140375595+TreesWise@users.noreply.github.com>
2024-03-06 18:18:46 +0530

    Update function_app.py
    changed time to 12 pm IST

--- function_app.py
+++ function_app.py
@@ -39,7 +39,7 @@ import requests
 app = func.FunctionApp()
 
 @app.function_name(name="mytimer")
-@app.schedule(schedule="0 20 13 * * *", 
+@app.schedule(schedule="0 30 6 * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
